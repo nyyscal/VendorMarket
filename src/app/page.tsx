92ae0a10a0c233ace9320a52366bb32ca70d1a33
Nischal <nyyscal541556@gmail.com>
2025-11-16 09:13:47 +0545

    Neobrutalism Effect CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return(
-    <div>
-     <p className="text-red-500">Hello World</p>
+    <div className="p-4">
+    <div className="flex flex-col py-4">
+     <div>
+      <Button variant={"elevated"}>I am a button</Button>
+      <Input placeholder="I am input"/>
+      <Progress value={50}/>
+      <Textarea placeholder={"I am text area."}/>
+      <Checkbox/>
+     </div>
+    </div>
     </div>
   )
 }
